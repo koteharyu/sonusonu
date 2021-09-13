f5eb6a32cdb1a1204ba89924b12b965b2cc9987b
haryu <hryau6@gmail.com>
2021-09-13 17:32:27 +0900

    add: ログイン機能

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,10 +5,15 @@ class Admin::SessionsController < ApplicationController
   end
 
   def create
-
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_back_or_to admin_login_path, success: "管理者としてログイン中..."
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+    end
   end
 
   def destroy
-    
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
-    get '/login', 'sessions#new'
-    post '/login', 'sessions#create'
-    delete '/logout', 'sessions#destroy'
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
   end
 end
